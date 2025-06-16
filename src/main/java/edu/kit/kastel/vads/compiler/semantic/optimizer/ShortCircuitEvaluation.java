20cc00050f0ce694a2bac618c7a7d8e5ba575a9f
Elscrux <nickposer2102@gmail.com>
2025-06-16 21:16:37 +0200

    fix: use recursive results from short circuit evaluation to support multiple nested replacements with ternaries

--- src/main/java/edu/kit/kastel/vads/compiler/semantic/optimizer/ShortCircuitEvaluation.java
+++ src/main/java/edu/kit/kastel/vads/compiler/semantic/optimizer/ShortCircuitEvaluation.java
@@ -2,6 +2,7 @@ package edu.kit.kastel.vads.compiler.semantic.optimizer;
 
 import edu.kit.kastel.vads.compiler.parser.ast.Tree;
 import edu.kit.kastel.vads.compiler.parser.ast.expression.BooleanTree;
+import edu.kit.kastel.vads.compiler.parser.ast.expression.ExpressionTree;
 import edu.kit.kastel.vads.compiler.parser.ast.expression.operation.BinaryOperationTree;
 import edu.kit.kastel.vads.compiler.parser.ast.expression.operation.TernaryOperationTree;
 import edu.kit.kastel.vads.compiler.parser.visitor.NodeReplacementVisitor;
@@ -10,13 +11,14 @@ import edu.kit.kastel.vads.compiler.parser.visitor.Unit;
 public class ShortCircuitEvaluation implements NodeReplacementVisitor<Unit> {
     @Override
     public Tree visit(BinaryOperationTree binaryOperationTree, Unit data) {
+        var left = (ExpressionTree) visit(binaryOperationTree.lhs(), data);
+        var right = (ExpressionTree) visit(binaryOperationTree.rhs(), data);
+
         switch (binaryOperationTree.operatorType()) {
             case AND -> {
                 // Use ternary operator for short-circuit evaluation
                 // A && B becomes A ? B : false
 
-                var left = binaryOperationTree.lhs();
-                var right = binaryOperationTree.rhs();
                 return new TernaryOperationTree(left,
                     right,
                     new BooleanTree(false, left.span().merge(right.span())));
@@ -25,8 +27,6 @@ public class ShortCircuitEvaluation implements NodeReplacementVisitor<Unit> {
                 // Use ternary operator for short-circuit evaluation
                 // A || B becomes A ? true : B
 
-                var left = binaryOperationTree.lhs();
-                var right = binaryOperationTree.rhs();
                 return new TernaryOperationTree(left,
                     new BooleanTree(true, left.span().merge(right.span())),
                     right);
